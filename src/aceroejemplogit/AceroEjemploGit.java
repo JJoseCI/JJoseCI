9b348a36eb9a98e2966a5a9aa619b782c4aab017
damt106 <damt106@36-11.aglinformatica.es>
2022-05-05 18:14:01 +0200

    version 1.0 Antes de refactorizar

---
+++ src/aceroejemplogit/AceroEjemploGit.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aceroejemplogit;
+
+/**
+ *
+ * @author damt101
+ */
+public class AceroEjemploGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Jose Maria Chacon");
+        System.out.println("Version 1");
+        System.out.println("Version 2");
+        // TODO code application logic here
+    }
+    
+}
